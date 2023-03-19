818022c3615daf1937cf5c4e66b90394dfd29fbc
luke francis <lukedevfrancis@gmail.com>
2023-03-19 18:22:21 -0400

    changed Models to Model

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
-class Book(models.Models):
+class Book(models.Model):
     title = models.CharField(max_length=40)
     rating = models.IntegerField(
         default = 1,
