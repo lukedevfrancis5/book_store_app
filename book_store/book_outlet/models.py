58b7126463b32b91e373ea3a873063c3b955c4d0
luke francis <lukedevfrancis@gmail.com>
2023-03-19 15:47:00 -0400

    added book class

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Book(models.Models):
+    title = models.CharField(max_length=40)
+    rating = models.IntegerField(
+        default = 1,
+        validators= [
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+    )
+    
