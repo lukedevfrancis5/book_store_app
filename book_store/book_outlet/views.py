7e62b5fb92bb007057cdc6eb2bf4a8d73c148d80
luke francis <lukedevfrancis@gmail.com>
2023-03-23 08:32:43 -0400

    added attributes

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": books
+    })
+
+def book_detail(request, id):
+    book = Book.objects.get(pk=id)
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling 
+    })
