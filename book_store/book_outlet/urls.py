bebdb3ebc53a001b0097ea29c670dda8dcf5f4b8
luke francis <lukedevfrancis@gmail.com>
2023-03-23 08:31:20 -0400

    added id to path

--- book_store/book_outlet/urls.py
+++ book_store/book_outlet/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views 
 
 
-url_patterns = [
-    path("", views.index)
+urlpatterns = [
+    path("", views.index),
+    path("<int:id>", views.book_detail)
 ]
