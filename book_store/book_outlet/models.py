dfd5c7f2b9ef353ae62854c83b5cba574242fb52
lukedevfrancis@gmail.com <lukedevfrancis@gmail.com>
2023-03-30 08:27:16 -0400

    return reverse function in to connect url

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse 
 
 # Create your models here.
 
@@ -10,5 +11,9 @@ class Book(models.Model):
     author = models.CharField(null=True, max_length=40)
     is_bestselling = models.BooleanField(default=False)
 
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
+
     def __str__(self):
         return f"{self.title} ({self.rating})"
+    
