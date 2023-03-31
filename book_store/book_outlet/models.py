59e7306f6945f41a8e5b5c1feb3d0fdf7999a574
lukedevfrancis@gmail.com <lukedevfrancis@gmail.com>
2023-03-31 09:40:06 -0400

    slugified save method

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.urls import reverse 
-
+from django.utils.text import slugify
 # Create your models here.
 
 class Book(models.Model):
@@ -10,9 +10,14 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=40)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False) # Harry Potter 1 => harry-potter-1
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id])
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
