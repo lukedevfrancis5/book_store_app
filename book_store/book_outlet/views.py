95e68f533a6e54bdc1bcbc4a2c5978cc3cb1d9ef
lukedevfrancis@gmail.com <lukedevfrancis@gmail.com>
2023-04-02 13:28:45 -0400

    added slugz to parameter

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
-
+from django.http import Http404
 # Create your views here.
 
 def index(request):
@@ -9,8 +9,12 @@ def index(request):
         "books": books
     })
 
-def book_detail(request, id):
-    book = Book.objects.get(pk=id)
+def book_detail(request, slug):
+    #try:
+    #     book = Book.objects.get(pk=id)
+    #except:
+    #     raise Http404 '''
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
         "author": book.author,
