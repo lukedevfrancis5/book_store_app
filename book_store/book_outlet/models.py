a60df38bfa55babe48961a3713009b7ca93d849d
lukedevfrancis@gmail.com <lukedevfrancis@gmail.com>
2023-04-02 13:43:00 -0400

    made slug field a db index

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=40)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False) # Harry Potter 1 => harry-potter-1
+    slug = models.SlugField(default="", null=False, db_index=True) # Harry Potter 1 => harry-potter-1
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
