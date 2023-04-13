a23ae31746fa4e353127d461f2b9b089044f1ba8
luke francis <lukedevfrancis@gmail.com>
2023-04-12 20:26:05 -0400

    made 2 functions

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -10,6 +10,13 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.full_name()
+
+
 # Book class with attributes
 
 class Book(models.Model):
